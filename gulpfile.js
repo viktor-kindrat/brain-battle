dc45985df5a7205bdff3b27390f66176922def4a
Viktor Kindrat <kindratvictor07@gmail.com>
2023-06-20 15:39:56 +0300

    inital setups

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const webp = require('gulp-webp');
+
+function compileStyles() {
+    return gulp.src(['./**/*.scss', '!./node_modules/**'])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./'));
+}
+
+function convertImg() {
+    return gulp.src(['./**/*.jpg', './**/*.png', './**/*.jpeg', '!./node_modules/**'])
+        .pipe(webp())
+        .pipe(gulp.dest('./'))
+}
+
+exports.compileStyles = compileStyles;
+
+exports.watchSass = function() {
+    gulp.watch(['./src/**/*.scss'], gulp.series('compileStyles'));
+};
+exports.imgConvert = convertImg
---
+++ src/Components/App/App.jsx
@@ -0,0 +1,11 @@
+import './Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      APP component
+    </div>
+  );
+}
+
+export default App;
