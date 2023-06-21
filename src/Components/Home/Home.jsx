1ea1e32b48c69a4135ef29b1230f63b0fcfb9a83
Viktor Kindrat <kindratvictor07@gmail.com>
2023-06-21 16:38:56 +0300

    UX fixes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,7 +4,7 @@ function Home (){
     return (
         <section className="Home">
             <div className="Home__content-container">
-                <h1 className="Home__headline">Conquer the Battle!</h1>
+                <h1 className="Home__headline">Brain Battle -<br /> The best kahoot platform</h1>
                 <div className="Home__button-container">
                     <button className="Home__button Home__button_filled">Join test</button>
                     <button className="Home__button Home__button_transparent">Create your test</button>
