678fb1ce4a27768418adf5617a290f9681c406f5
Viktor Kindrat <kindratvictor07@gmail.com>
2023-06-23 16:47:47 +0300

    Google auth link setup

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -8,7 +8,7 @@ function Form({ type }) {
             <div className="Form__content-container">
                 <h2 className="Form__headline">{(type === "reg" ? "Create an account" : "Log in")}</h2>
                 <div className="Form__fields">
-                    <a href="https://brain-battle-server.onrender.com/auth/google" className="Form__button"> <img className="Form__btn-icon" height={25} src={googleIcon} alt="google icon" />Continue with Google</a>
+                    <a href="https://brain-battle-server-wpcm.onrender.com/auth/google" className="Form__button"> <img className="Form__btn-icon" height={25} src={googleIcon} alt="google icon" />Continue with Google</a>
                     {
                         (type === "log") ? <>
                             <div className="Form__fields-container">
