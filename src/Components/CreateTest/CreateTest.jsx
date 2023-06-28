8eee1500f36eb337cf0fbcb57d2cf72ca16f1d1b
Viktor Kindrat <kindratvictor07@gmail.com>
2023-06-28 23:13:02 +0300

    test creation layout styleing (part 1/2)

--- src/Components/CreateTest/CreateTest.jsx
+++ src/Components/CreateTest/CreateTest.jsx
@@ -1,9 +1,12 @@
+import "./Styles/CareateTest.css"
+
+import addIcon from "./Svg/add.svg"
+
 import { useReducer } from "react"
 import quesReducer from "./Reducers/questionsReducer";
 
 import QuestionField from "../QuestionField/QuestionField";
 
-import "./Styles/CareateTest.css"
 
 function CreateTest({ logined, setLogined }) {
     let [questions, questionsDispatch] = useReducer(quesReducer, [{
@@ -21,8 +24,8 @@ function CreateTest({ logined, setLogined }) {
         <section className="CreateTest">
             <h2 className="CreateTest__headline">Create new test</h2>
             <div className="CreateTest__group">
-                <input className="CreateTest__name" type="text" defaultValue="Untitled" />
-                <textarea className="CreateTest__decription" defaultValue="My best test"></textarea>
+                <input className="CreateTest__name" type="text" placeholder="Name of the test here*"  defaultValue="Untitled" />
+                <textarea className="CreateTest__decription" placeholder="Description*" defaultValue="My best test"></textarea>
             </div>
             <div className="CreateTest__questions">
                 <h3 className="CreateTest__headline CreateTest__headline_3">Questions</h3>
@@ -33,7 +36,7 @@ function CreateTest({ logined, setLogined }) {
                         )
                     }
                 </div>
-                <button onClick={addQuestionHandler} className="CreateTest__questions-button">Add more question</button>
+                <button onClick={addQuestionHandler} className="CreateTest__questions-button"><img height={20} src={addIcon} alt="add" />Add more question</button>
             </div>
         </section>
     )
--- src/Components/QuestionField/QuestionField.jsx
+++ src/Components/QuestionField/QuestionField.jsx
@@ -1,5 +1,8 @@
 import "./Styles/QuestionField.css"
 
+import deleteIcon from "./Svg/trash.svg"
+import addIcon from "./Svg/add.svg"
+
 import QuestionVariant from "../QuestionVariant/QuestionVariant"
 
 function QuestionField({ id, question, questionsDispatch }) {
@@ -28,13 +31,13 @@ function QuestionField({ id, question, questionsDispatch }) {
     return (
         <div key={id} className="QuestionField" data-id={id}>
             <div className="QuestionField__head">
-                <input onChange={changeQuestionHandler} className="QuestionField__input" type="text" value={question.text} placeholder="Enter the question" />
-                <button onClick={removeQuestionHandler} className="QuestionField__delete">remove</button>
+                <input onChange={changeQuestionHandler} className="QuestionField__input" type="text" value={question.text} placeholder="Ask respondents about something" />
+                <button onClick={removeQuestionHandler} className="QuestionField__delete"><img className="QuestionField__delete-icon" height={25} src={deleteIcon} alt="remove" /></button>
             </div>
             <div className="CreateTest__questions-variants">
                 {question.variants.map((item, index) => <QuestionVariant {...{ questionsDispatch, item, index, id }} />)}
             </div>
-            <button className="CreateTest__questions-variants-button" onClick={addVariantHandler}>Add more variants</button>
+            <button className="CreateTest__questions-variants-button" onClick={addVariantHandler}> <img src={addIcon} alt="add" />More variant</button>
         </div>
     )
 }
