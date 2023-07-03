ef3c327fc6723dcf5d2bfa425bb7d379c196d680
Viktor Kindrat <kindratvictor07@gmail.com>
2023-07-03 13:15:23 +0300

    responsove layout setup

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -8,10 +8,10 @@ import addIcon from "./Svg/add.svg"
 import DashboardTestCard from "../DashboardTestCard/DashboardTestCard";
 import Loader from "../Loader/Loader"
 
-function Dashboard({logined, setLogined, userData, setInvokeStatus}) {
+function Dashboard({ logined, setLogined, userData, setInvokeStatus }) {
     return (
         <div className="Dashboard">
-            <Loader visibility={Object.keys(userData).length === 0}/>
+            <Loader visibility={Object.keys(userData).length === 0} />
             <aside className="Dashboard__user-info">
                 <h2 className="Dashboard__headline">Account information</h2>
                 <div className="Dashboard__personal-info">
@@ -30,30 +30,29 @@ function Dashboard({logined, setLogined, userData, setInvokeStatus}) {
                 </div>
                 <div className="Dashboard__test-container">
                     {
-                        userData.tests && userData.tests.length > 0 ? 
-                        userData.tests.map((test, index)=>{
-                            console.log(test)
-                            let success = Math.round(
-                                test.testings.map((item) => {
-                                  return (
-                                    item.respondents
-                                      .map((resp) => {
+                        userData.tests && userData.tests.length > 0 ?
+                            userData.tests.map((test, index) => {
+                                console.log(test)
+                                let success = Math.round(
+                                    test.testings.map((item) => {
                                         return (
-                                          Math.round(
-                                            (resp.answers.reduce((acc, value) => value === "true" ? acc += 1 : acc, 0) / resp.answers.length) * 100
-                                          )
+                                            item.respondents
+                                                .map((resp) => {
+                                                    return (
+                                                        Math.round(
+                                                            (resp.answers.reduce((acc, value) => value === "true" ? acc += 1 : acc, 0) / resp.answers.length) * 100
+                                                        )
+                                                    );
+                                                })
+                                                .reduce((acc, value) => acc += value, 0) / item.respondents.length
                                         );
-                                      })
-                                      .reduce((acc, value) => acc += value, 0) / item.respondents.length
-                                  );
-                                }).reduce((acc, value) => acc += value, 0) / test.testings.length
-                              );
-                              
-                            let countOfResps = test.testings.reduce((acc, value)=>acc += value.respondents.length, 0);
-                            return <DashboardTestCard key={index} test={test} id={test.id} name={test.name} description={test.description} questions={test.questions.length} countOfStudents={countOfResps === 0 ? "soon" : countOfResps} success={success !== 0 ? success + "%" : "soon"}/>
-                        }
+                                    }).reduce((acc, value) => acc += value, 0) / test.testings.length
+                                );
+                                let countOfResps = test.testings.reduce((acc, value) => acc += value.respondents.length, 0);
+                                return <DashboardTestCard key={index} test={test} id={test.id} name={test.name} description={test.description} questions={test.questions.length} countOfStudents={countOfResps === 0 ? "soon" : countOfResps} success={success !== 0 ? success + "%" : "soon"} />
+                            }
                             )
-                        : <p className="Dashboard__error">You haven't any tests yet</p>
+                            : <p className="Dashboard__error">You haven't any tests yet</p>
                     }
                 </div>
             </section>
