de9db5122ef675f398ce56615c724891519ae85f
Viktor Kindrat <kindratvictor07@gmail.com>
2023-07-04 12:31:57 +0300

    settings menu styleing

--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -6,12 +6,22 @@ function Settings({ userData, setUserData, logined, setInvokeStatus, invokeStatu
     return (
         <section className="Settings">
             <h2 className="Settings__headline">Settings</h2>
-            <Link to="/settings/">account</Link>
-            <Link to="/settings/security">security</Link>
-            <Routes>
-                <Route path="/" element={<div>Account Settings</div>} />
-                <Route path="security" element={<div>Security settings</div>} />
-            </Routes>
+            <div className="Settings__content">
+                <div className="Settings__menu">
+                    <Link to="/settings/">
+                        <button className="Settings__menu-btn">Account</button>
+                    </Link>
+                    <Link to="/settings/security">
+                        <button className="Settings__menu-btn">Security</button>
+                    </Link>
+                </div>
+                <div className="Settings__detail">
+                    <Routes>
+                        <Route path="/" element={<div>Account Settings</div>} />
+                        <Route path="security" element={<div>Security settings</div>} />
+                    </Routes>
+                </div>
+            </div>
         </section>
     )
 }
